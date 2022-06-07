a2b98bce78dc4a9d938e06a4460dcc3904f62b09
Santiago Xiao <santixiaorl@gmail.com>
2022-06-07 11:09:15 +0200

    home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,67 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
 
-const Home = () =>{
-    return(
-        <>
-            <h1>home page</h1>
-            <Link to="survivors">Survivors</Link>
-            <Link to="killers">Killers</Link>
-        </>
-    ) 
-}
+const Home = () => {
+  return (
+    <>
+      <div className="homecontainer">
+        <h1>Welcome to Store By Daylight</h1>
+        <p className="subtitle">
+          Store by Daylight is a store for playable characters from the video
+          game Dead By Daylight
+        </p>
+        <div className="textconatiner">
+          <p>
+            <span className="boldfont">
+              Dead by Daylight is a survival horror asymmetric multiplayer
+              online game&nbsp;
+            </span>
+            developed by Behaviour Interactive. It is a one-versus-four game in
+            which{" "}
+            <span className="boldfont">
+              one player takes on the role of a savage killer and the other four
+              play as survivors
+            </span>
+            ; the killer must{" "}
+            <span className="boldfont">
+              catch each survivor and sacrifice them
+            </span>{" "}
+            to a malevolent force known as the Entity by impaling them on hooks,
+            while the survivors must{" "}
+            <span className="boldfont">
+              avoid being caught and fix five generators
+            </span>{" "}
+            to open the exit gates.
+          </p>
+        </div>
+      </div>
+      <div className="videocontainer">
+        <iframe
+          title="homevideo"
+          width="600"
+          height="400"
+          frameBorder="0"
+          src="https://www.youtube.com/embed/JGhIXLO3ul8?autoplay=1&mute=1&loop=1"
+          className="homecontainer"
+        ></iframe>
+      </div>
+      <div className="homecontainer title">
+        <h2>Find a way out as a survivor... or hunt as a killer</h2>
+        <img
+          src={require("../images/trapperbanner.webp")}
+          alt="banner"
+          className="banner"
+        />
+      </div>
+      <div className="homecontainer">
+        <h2 className="title">
+          Different styles of play. Choose the one that best suits yours
+        </h2>
+        <p style={{ textAlign: "center" }}>
+          From map controllers, silent, invisible... to fast and deadly
+        </p>
+      </div>
+    </>
+  );
+};
 
 export default Home;
